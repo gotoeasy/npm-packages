c96e181bfdb2dcc2818a4a9bbb124ad16372c552
0090mz <m.zhang@mbpsoft.com>
2018-09-13 19:18:00 +0900

    1

--- gotoeasy-file/index.js
+++ gotoeasy-file/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const anymatch = require('anymatch');
 
 // 按指定编码写文件
 // file为对象时，取属性使用：path、file、text、encoding
@@ -62,10 +61,14 @@ function remove(path) {
 };
 
 // 不传matchers时不查询子目录
-// matchers: 类似gitignore，*表示不含目录分隔符的任意字符，**表示任意字符含任意多级目录，?表示一个字符
+// matchers: 匹配规则
+// [?]  - 单一字符
+// [*]  - 除目录分隔符[/]以外的任意字符
+// [**] - 任意字符
+// [!]  - 感叹号开头时表示排除
 function files(path, ...matchers) {
 	if ( matchers.length ) {
-		return listFiles(path, matchers);
+		return listFiles(path.replace(/\\/g, '/'), matchers);
 	}
 
 	let rs = [];
@@ -74,21 +77,114 @@ function files(path, ...matchers) {
 		ls.forEach((file, index) => rs.push(path + "/" + file));
 	}
 	return rs;
-};
+}
 
 function listFiles(path, matchers) {
+	//console.info(path, matchers)
+	let result = [];
+	findFiles(path, getMatcher(path, matchers), result);
+	return result;
+}
 
-	let matcher = {
-		match: function (file){
-			let name = file.replace(path + '/', '');
-			return anymatch(matchers, name);
+function getMatcher(path, matchers){
+
+	let includes = [];
+	let excludes = [];
+	for ( let i=0; i<matchers.length; i++ ) {
+		let match = matchers[i].replace(/\\/g, '/').trim();
+		let isExclude = false;
+		let isDir = false;
+		if ( match.startsWith('!') ) {
+			isExclude = true;
+			match = match.substring(1);
 		}
+		if ( match.startsWith('/') ) {
+			match = match.substring(1);
+		}
+		if ( match.endsWith('/') ) {
+			isDir = true;
+			match = match.substring(0, match.length-1);
+		}
+		if ( match == '' ) {
+			continue;
+		}
+
+		let reg = {
+			isDir: isDir,
+			reg: new RegExp(convert(match))
+		}
+		isExclude ? excludes.push(reg) : includes.push(reg);
+	}
+
+	let matcher = (file, isDir) => {
+		let target = file.replace(/\\/g, '/');
+		if ( !target.startsWith(path) ) {
+			return false;
+		}
+		target = target.substring(path.length);
+		let match;
+
+		// 排除规则先做
+		for ( let i=0; i<excludes.length; i++ ) {
+			match = excludes[i];
+			if ( match.reg.test(target) ) { // 匹配
+				if ( match.isDir ) {
+					if ( isDir ) {
+						return false; // 当前是目录,是排除对象
+					}
+				}else{
+					return false; // 排除的文件或目录
+				}
+			}
+		}
+
+		// 匹配规则后做
+		for ( let i=0; i<includes.length; i++ ) {
+			match = includes[i];
+			if ( match.reg.test(target) ) { // 只要文件，不用考虑目录，直接匹配
+				return true;
+			}
+		}
+
+		if ( isDir ) {
+			return true; // 目录,继续找
+		}
+		return false;
 	};
 
-	let result = [];
-	findFiles(path, matcher, result);
-	return result;
-};
+	return matcher;
+}
+
+function convert(match){
+	// [?]  - 单一字符
+	// [*]  - 除目录分隔符[/]以外的任意字符
+	// [**] - 任意字符
+	let reg = match;
+
+	reg = reg.replace(/\^/g, '\\^');
+	reg = reg.replace(/\$/g, '\\$');
+	reg = reg.replace(/\./g, '\\.');
+	reg = reg.replace(/\+/g, '\\+');
+	reg = reg.replace(/\-/g, '\\-');
+	reg = reg.replace(/\=/g, '\\=');
+	reg = reg.replace(/\!/g, '\\!');
+	reg = reg.replace(/\(/g, '\\(');
+	reg = reg.replace(/\)/g, '\\)');
+	reg = reg.replace(/\[/g, '\\[');
+	reg = reg.replace(/\]/g, '\\]');
+	reg = reg.replace(/\{/g, '\\{');
+	reg = reg.replace(/\}/g, '\\}');
+
+	reg = reg.replace(/\?/g, '.');			// [.]  - 单个问号，替换为查找单个字符
+	reg = reg.replace(/\*{2,}/g, '\\S\\n');	// [\S] - 两个以上星号，替换为查找非空白字符
+	reg = reg.replace(/\*{1}/g, '[^/]*');	// [^/] - 单个星号，替换为查找目录分隔符[/]以外的任意字符
+
+	reg = reg.replace(/\\n/g, '*');
+
+	reg = '^' + reg + '$';
+	//console.info(reg)
+	return reg;
+}
 
 function findFiles(path, matcher, result){
 
@@ -97,10 +193,10 @@ function findFiles(path, matcher, result){
 		let absName = path + "/" + file;
 		if ( isFile(absName) ) {
 			// 文件，匹配保存
-			matcher.match(absName) && result.push(absName);
+			matcher(absName) && result.push(absName);
 		}else{
 			// 目录，继续找
-			findFiles(absName, matcher, result);
+			matcher(absName, true) && findFiles(absName, matcher, result);
 		}
 	});
 }
