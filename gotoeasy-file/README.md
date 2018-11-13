e8b072ec816e7709a268ca96665f4c0d5f6b8d24
0090mz <m.zhang@mbpsoft.com>
2018-11-13 19:06:22 +0900

    1

--- gotoeasy-file/README.md
+++
@@ -1,9 +0,0 @@
-# gotoeasy-file
-常用文件操作封装
-<br>
-<br>
-
-[![NPM version](https://img.shields.io/npm/v/gotoeasy-file.svg)](https://www.npmjs.com/package/gotoeasy-file)
-[![License](https://img.shields.io/badge/License-Apache%202-brightgreen.svg)](http://www.apache.org/licenses/LICENSE-2.0)
-<br>
-<br>
--- gotoeasy-javaformatter/index.js
+++
@@ -1,9 +0,0 @@
-const prc = require('gotoeasy-process');
-
-function format(...files){
-	let java = process.env.JAVA_HOME ? process.env.JAVA_HOME + '/bin/java' : 'java';
-	let cmd = java + ' -jar "' + __dirname + '/javaformatter.jar" ' + files.join(' ');
-	prc.exec(cmd);
-};
-
-module.exports = format;
