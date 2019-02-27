325b740d0cf7c935629330d32ad72ce0db799a10
gotoeasy <gotoeasy@163.com>
2019-02-27 18:24:01 +0900

    1

--- csslibify/coverage.js
+++
@@ -1,3 +0,0 @@
-const opn = require('opn');
-
-opn('coverage/index.html'); // 浏览器本地打开查看覆盖率报告
--- csslibify/lib/m300-get-split-postcss-plugins.js
+++
@@ -1,8 +0,0 @@
-const bus = require('@gotoeasy/bus');
-
-module.exports = bus.on('get-split-postcss-plugins', function(){
-
-    return () => bus.on('split-plugins');
-
-}());
-
