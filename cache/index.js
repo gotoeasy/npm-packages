099e58553b29a11dacfdbb90bc1b86f390f4b925
gotoeasy <gotoeasy@163.com>
2019-04-04 17:02:34 +0900

    1

--- cache/index.js
+++ cache/index.js
@@ -1,62 +1,97 @@
 const os = require('@gotoeasy/os');
-const hash = require('@gotoeasy/hash');
-const flatCache = require('flat-cache');
+const File = require('@gotoeasy/file');
 
-// 命名缓存，存盘
-const map = new Map();
+// TODO 过期
+class Cache extends Map {
+	constructor(opts={}) {
+		super();
+		this.path = opts.path;
+		this.max = opts.max;
+	}
 
-// 默认缓存，不存盘
-const defaultCache = flatCache.load('default');
-defaultCache.put = (key, value) => defaultCache.setKey(key, value);
-defaultCache.remove = key => defaultCache.removeKey(key);
-defaultCache.get = (key, defaultValue) => {
-    let value = defaultCache.getKey(key);
-    return value !== undefined ? value : defaultValue;
-};
-defaultCache.save = () => {};
+	peek(key) {
+        let oData = super.get(key);
+        if ( !oData ) {
+            oData = hashKey(key, this.path);
+            if ( !File.existsFile(oData.file) ) return undefined;
+            oData = JSON.parse(File.read(oData.file));
+        }
+        return oData.value;
+	}
 
-process.on('exit', e => {
-    map.forEach(cache => cache.save(true))
-})
+	set(key, value) {
+        let oData = super.get(key);
+        if ( !oData ) {
+            oData = hashKey(key, this.path);
+            oData.value = value;
+            super.size >= this.max && super.delete(super.keys().next().value);
+            super.set(key, oData);
+        } else {
+            oData.value = value;
+            super.delete(key) > super.set(key, oData);
+        }
 
-function load(name, opts={}) {
-    if ( name == null ) {
-        return defaultCache;
+        setTimeout( ()=>File.write(oData.file, JSON.stringify(oData)) );                // 存盘
+		return value;
+	}
+
+	delete(key) {
+        let oData = super.get(key) || hashKey(key, this.path);
+        File.remove(oData.file);                                                        // 删除缓存文件
+        return super.delete(key);                                                       // 删除缓存
+	}
+
+	get(key) {
+        let oData = super.get(key);
+        if ( !oData ) {
+            oData = hashKey(key, this.path);
+            if ( File.existsFile(oData.file) ) {
+                oData = JSON.parse(File.read(oData.file));
+                super.size >= this.max && super.delete(super.keys().next().value);
+                super.set(key, oData);
+                return oData.value;
+            }
+            return undefined;       // ...
+        }
+
+        super.delete(key) > super.set(key, oData);
+        return oData.value;
+	}
+}
+
+function hashKey(key, path){
+    // 53653:   00000000 11010001 10010101
+    // 863803:  00001101 00101110 00111011
+    // 5139113: 01001110 01101010 10101001
+	let rs1 = 53653, rs2 = 863803, rs3 = 5139113, i = (key == null ? 0 : key.length), charcode;
+	while ( i ) {
+        charcode = key.charCodeAt(--i);
+		rs1 = (rs1 * 33) ^ charcode;
+		rs2 = (rs2 * 33) ^ charcode;
+		rs3 = (rs3 * 33) ^ charcode;
+	}
+    rs1 = rs1 >>> 0;
+    rs2 = rs2 >>> 0;
+    rs3 = rs3 >>> 0;
+
+    let p1 = rs1 & 0b11111111;
+    let p2 = (rs1 & 0b1111111100000000) >>> 8;
+    let p3 = (rs1 & 0b111111110000000000000000) >>> 16;
+    
+    let file = require('path').resolve(path, `${p3}/${p2}/${p1}/${rs1.toString(36)}-${rs2.toString(36)}-${rs3.toString(36)}.json`);
+	return {file};
+}
+
+module.exports = (function (oCaches={}){
+
+    return (opts={}) => {
+        let name = opts.name || 'default';
+        let path = require('path').resolve(opts.path || (os.homedir() + '/.cache'), name).replace(/\\/g, '/');
+		let max = opts.max > 0 && opts.max || 10000;
+        if ( !oCaches[path] ) {
+            oCaches[path] = new Cache({path, max});
+        }
+        return oCaches[path];
     }
 
-    opts.disableDiskCache && (name = hash(name));
-
-    if ( map.has(name) ) {
-        return map.get(name);
-    }
-
-    let cachePath = opts.path || (os.homedir() + '/.gotoeasy/.cache');
-    let oCache = flatCache.load(name, cachePath);
-    oCache.disableDiskCache = opts.disableDiskCache;
-    oCache.put = (key, value) => oCache.setKey(key, value);
-    oCache.remove = key => oCache.removeKey(key);
-    oCache.get = (key, defaultValue) => {
-        let value = oCache.getKey(key);
-        return value !== undefined ? value : defaultValue;
-    };
-    oCache.fnSave = oCache.save;
-    oCache.save = yes => !opts.disableDiskCache && oCache.fnSave(yes);
-
-    map.set(name, oCache);
-    return oCache;
-}
-
-function put(key, value) {
-    defaultCache.put(key, value);
-}
-
-function get(key, defaultValue) {
-    return defaultCache.get(key, defaultValue);
-}
-
-function remove(key) {
-    defaultCache.remove(key);
-}
-
-module.exports = {put: put, get: get, remove: remove, load};
-
+})();
