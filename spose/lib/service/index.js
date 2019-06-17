6084bcb407a0142b32ce9b1aa25fa87096feaef1
gotoeasy <gotoeasy@163.com>
2019-06-17 19:17:55 +0900

    1

--- spose/lib/service/index.js
+++ spose/lib/service/index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 module.exports = function (opts){
 
-    let btf = new Btf(File.resolve(__dirname, 'template.eureka.greenwich.btf'));
+    let btf = new Btf(File.resolve(__dirname, 'template.service.greenwich.btf'));
     let oConf = getConfig(opts);
 
     let file, text;
@@ -14,14 +14,14 @@ module.exports = function (opts){
     // 启动程序
     text = btf.getText('SpringBootApplication');
     text = text.replace(/%package%/g, package);
-    file = `${opts.cwd}/src/main/java/${pkgPath}/EurekaServerApplication.java`;
+    file = `${opts.cwd}/src/main/java/${pkgPath}/ServiceApplication.java`;
     File.write(file, text);
     console.log('generate ......', file);
 
     // 测试程序
     text = btf.getText('SpringBootApplicationTests');
     text = text.replace(/%package%/g, package);
-    file = `${opts.cwd}/src/test/java/${pkgPath}/EurekaServerApplicationTests.java`;
+    file = `${opts.cwd}/src/test/java/${pkgPath}/ServiceApplicationTests.java`;
     File.write(file, text);
     console.log('generate ......', file);
 
@@ -30,6 +30,8 @@ module.exports = function (opts){
     text = text.replace(/%service-name%/g, oConf['service-name']);
     text = text.replace(/%server-host%/g, oConf['server-host']);
     text = text.replace(/%server-port%/g, oConf['server-port']);
+
+    text = text.replace(/%register-center-url%/g, oConf['register-center-url']);
     file = opts.cwd + '/src/main/resources/application.yml';
     File.write(file, text);
     console.log('generate ......', file);
@@ -55,8 +57,8 @@ module.exports = function (opts){
 
 function getConfig(opts){
 
-    let fileConfDefault = File.resolve(__dirname, 'spose.eureka.config.btf');
-    let fileConfInput = File.resolve(opts.cwd, 'spose.eureka.config.btf');
+    let fileConfDefault = File.resolve(__dirname, 'spose.service.config.btf');
+    let fileConfInput = File.resolve(opts.cwd, 'spose.service.config.btf');
     if ( !File.existsFile(fileConfInput) ) {
         fs.copyFileSync(fileConfDefault, fileConfInput);    // 无配置文件时，默认复制一份便于确认
     }
