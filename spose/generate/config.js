c6af57fae469ef667ebc17a5c31ab36d271ca618
gotoeasy <gotoeasy@163.com>
2019-06-26 18:59:59 +0900

    greenwich-config

--- spose/generate/config.js
+++ spose/generate/config.js
@@ -2,14 +2,17 @@ const File = require('@gotoeasy/file');
 const fs = require('fs');
 const execa = require('execa');
 
+const NAME = 'config';
+const JAR = `greenwich-${NAME}-1.0.0.jar`;
+
 module.exports = function (opts){
 
     console.info('------------------------------');
-    console.info('    build greenwich-config');
+    console.info(`    build greenwich ${NAME}`);
     console.info('------------------------------');
 
-    let dir = File.resolve(__dirname, '../greenwich/config');
-    let dist = File.resolve(opts.cwd, 'config');
+    let dir = File.resolve(__dirname, `../greenwich/${NAME}`);
+    let dist = File.resolve(opts.cwd, NAME);
 
     // 复制源文件
     if ( !File.existsFile(dist + '/build.gradle') ) {
@@ -23,25 +26,26 @@ module.exports = function (opts){
     }
 
     // 编译
-    let cwd = opts.cwd + '/config';
+    let cwd = `${opts.cwd}/${NAME}`;
     let rs = execa.sync('gradle', ['clean', 'build'], {cwd});
     console.log(rs.stderr || rs.stdout);
 
     // 复制jar
-    let buildjarfile = opts.cwd + '/config/build/libs/greenwich-config-1.0.0.jar';
-    let jarfile = opts.cwd + '/greenwich-config-1.0.0.jar';
+    let buildjarfile = `${opts.cwd}/${NAME}/build/libs/${JAR}`;
+    let jarfile = `${opts.cwd}/${JAR}`;
     fs.copyFileSync(buildjarfile, jarfile);
 
     // 写配置文件
-    if ( !File.existsFile(opts.cwd + '/config.properties') ) {
-        File.write(opts.cwd + '/config.properties', getProperties(1));
+    let propertiesfile = `${opts.cwd}/${NAME}.properties`;
+    if ( !File.existsFile(propertiesfile) ) {
+        File.write(propertiesfile, getProperties());
     }
 
     // 提示运行命令
     console.info('');
     console.info('etc.');
-    console.info('  java -jar greenwich-config-1.0.0.jar');
-    console.info('  java -jar greenwich-config-1.0.0.jar --spring.config.location=config.properties');
+    console.info(`  java -jar ${JAR}`);
+    console.info(`  java -jar ${JAR} --spring.config.location=${NAME}.properties`);
 
 }
 
@@ -49,12 +53,12 @@ module.exports = function (opts){
 function getProperties(){
 
     return `# 服务名
-spring.application.name=greenwich-config
+spring.application.name=config
 
 # 主机
 eureka.instance.hostname=localhost
 # 端口
-server.port=9101
+server.port=9201
 
 spring.cloud.config.server.git.uri=https://gitee.com/gotoeasy/config.git
 spring.cloud.config.server.git.searchPaths=greenwich/**
@@ -62,6 +66,8 @@ spring.cloud.config.server.git.searchPaths=greenwich/**
 # 列表上Status的显示格式
 eureka.instance.instance-id=\${eureka.instance.hostname}:\${server.port}
 
+# 开启健康检查，用以剔除失效节点
+eureka.client.healthcheck.enabled=true
 # 注册中心地址
 eureka.client.serviceUrl.defaultZone=http://eureka:eureka@localhost:8761/eureka/
 `;
