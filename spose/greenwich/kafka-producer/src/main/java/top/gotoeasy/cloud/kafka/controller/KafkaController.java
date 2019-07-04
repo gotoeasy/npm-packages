2456d8b002c3264c3069155949973312c21d4e8b
gotoeasy <gotoeasy@163.com>
2019-07-04 16:04:48 +0900

    kafka-producer

--- spose/greenwich/kafka-producer/src/main/java/top/gotoeasy/cloud/kafka/controller/KafkaController.java
+++ spose/greenwich/kafka-producer/src/main/java/top/gotoeasy/cloud/kafka/controller/KafkaController.java
@@ -1,7 +1,6 @@
 package top.gotoeasy.cloud.kafka.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +10,11 @@ import top.gotoeasy.cloud.kafka.service.KafkaProducerService;
 @RestController
 public class KafkaController {
 
-    @Value("${kafka.topic}")
-    private String               topic;
-
     @Autowired
     private KafkaProducerService kafkaProducerService;
 
     @RequestMapping("/send")
-    public boolean send(@RequestParam("message") String message) {
+    public boolean send(@RequestParam("topic") String topic, @RequestParam("message") String message) {
         return kafkaProducerService.send(topic, message);
     }
 
